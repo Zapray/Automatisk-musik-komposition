1dfe7adc138eff322358609bf3673125e1fa64b9
Johan Gronvall <Johan.gronvall.92@gmail.com>
2015-02-04 14:02:40 +0100

    Added som useful functinality to Note class

--- src/Note.java
+++ src/Note.java
@@ -2,6 +2,8 @@
 public class Note {
 	private int pitch;
 	private int duration;
+	public static int PITCHMAX = 14;
+	public static int DURATIONMAX = 16;
 	
 	public Note(int pitch, int duration) {
 		this.pitch = pitch;
@@ -14,6 +16,9 @@ public class Note {
 		return duration;
 	}
 	public boolean isNote() {
-		return pitch <= 14 && duration <= 16; 
+		return pitch <= PITCHMAX && duration <= DURATIONMAX; 
+	} //magic numbers!
+	public int getmatrixPosition() {
+		return pitch-1 + (duration-1)*PITCHMAX;
 	}
 }
