2f9bbf71ecb037d8a3cff76d0cd8f330a618485b
Albin Lorentzson <Albin@dhcp-187085.eduroam.chalmers.se>
2015-02-06 14:10:38 +0100

    Härligt

---
+++ src/MidiPlayer.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import javax.sound.midi.*;
+
+public class MidiPlayer {
+      
+      public static void main(String args[]){
+         
+         Bufferreader in;
+         
+         String line;
+         
+         in =new Bufferreader(new Filereader("\Users\Albin\Desktop\filename.txt"));
+         line=in.readLine();
+         
+         
+         while(line !=null){
+            
+         
+         
+         }
+        
+         
+         
+         
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      }
+
+
+
+
+
+
+
+
+}
