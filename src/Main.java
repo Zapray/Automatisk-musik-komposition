c50d4d6ebb335d28e4813b36b05ffae4945c9bca
Johan Gronvall <Johan.gronvall.92@gmail.com>
2015-02-20 16:27:49 +0100

    compiled project into a test.jar

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.List;
 
 
 public class Main {
-
-	private static int pMax, dMax, order = 2 , length = 20;
-	public static void main(String[] args){
-		
+	//In order to run, navigate to Automatisk-Musik-Komposition and type the following into a console
+	//java -jar myJar.jar
+	private static int pMax, dMax, order = 3 , length = 20;
+	public static void main(String[] args) {
 		MidiManager mm = new MidiManager(System.getProperty("user.dir")+"/databas.txt");
 		List<? extends List<Note>> l = mm.getData();
 		pMax = mm.getPMax();
@@ -21,4 +21,5 @@ public class Main {
 		}
 	
 	}
+	
 }
