673ecce6d1af105acb7495227d4ccd9aa79f1bf6
Johan Gronvall <johan.gronvall.92@gmail.com>
2015-04-05 13:11:59 +0200

    changes to SongGenerator

--- src/SongGenerator.java
+++ src/SongGenerator.java
@@ -1,12 +1,20 @@
 import java.util.List;
 
-
+/**
+ * Class in charge of putting a song together, given a structure and a generator.
+ * Deals with MotiveGeneration
+ * @author Zapray
+ *
+ */
 public class SongGenerator {
 
+	public SongGenerator() {
+		
+	}
+	
 	public List<Frame> generateSong(MelodyFrameGenerator tmn, List<Section> sections) {
 		return null;
 		// TODO Auto-generated method stub
-		
 	}
 
 }
