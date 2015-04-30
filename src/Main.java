722ddca0c110ba7d586e33d6efbed609bc1798c8
AlbinL <albin.lorentzson@me.com>
2015-04-30 10:14:43 +0200

    Lite skräp

--- src/Main.java
+++ src/Main.java
@@ -68,8 +68,13 @@ public class Main {
 		
 		List<Motive> mvl = new ArrayList<Motive>();  // = StructueGenerator.GenerateStructure(frames)
 		mvl.add(new Motive(1));
-		mvl.add(new Motive(1, false, Percentage.EIGHTY));
-		mvl.add(new Motive(1, false, Percentage.EIGHTY));
+		mvl.add(new Motive(2));
+		mvl.add(new Motive(1, false, Percentage.SIXTY));
+		mvl.add(new Motive(2, false, Percentage.EIGHTY));
+		mvl.add(new Motive(3));
+		mvl.add(new Motive(4));
+		mvl.add(new Motive(1, false, Percentage.SIXTY));
+		mvl.add(new Motive(2, false, Percentage.EIGHTY));
 		frames = mvl.size();//magic number!
 		MotiveGenerator mg = new MotiveGenerator();
 		ChordMarkov markov = new ChordMarkov(CHORDORDER, cMax, l);
